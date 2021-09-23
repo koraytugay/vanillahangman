cf449bf9b3e0237df9310be83473ff6eb1102612
koraytugay <ktugay@sonatype.com>
2021-09-22 21:28:44 -0400

    no need for iife in service

--- src/hangmanService.js
+++ src/hangmanService.js
@@ -1,39 +1,32 @@
-export default (function hangmanGame() {
+const pokemonNames = [];
 
-  const pokemonNames = [];
+function init() {
+  return fetch("https://pokeapi.co/api/v2/pokemon?limit=100")
+      .then(response => response.json())
+      .then(json => pokemonNames.push(...json['results'].flatMap(pokemon => pokemon.name)));
+}
 
-  function init() {
-    return fetch("https://pokeapi.co/api/v2/pokemon?limit=100")
-        .then(response => response.json())
-        .then(json => {
-          pokemonNames.push(
-              ...json['results'].flatMap(pokemon => pokemon.name)
-          );
-        });
-  }
-
-  function HangmanGame() {
-    this.secretWord = pokemonNames[Math.floor(Math.random() * 100)];
-    this.attempts = [];
-    this.unsuccessfulAttemptCount = 0;
-    this.attempt = function(letter) {
-      if (!this.attempts.includes(letter)) {
-        this.attempts.push(letter);
-        if (![...this.secretWord].includes(letter)) {
-          this.unsuccessfulAttemptCount++;
-        }
+function HangmanGame() {
+  this.secretWord = pokemonNames[Math.floor(Math.random() * 100)];
+  this.attempts = [];
+  this.unsuccessfulAttemptCount = 0;
+  this.attempt = function(letter) {
+    if (!this.attempts.includes(letter)) {
+      this.attempts.push(letter);
+      if (![...this.secretWord].includes(letter)) {
+        this.unsuccessfulAttemptCount++;
       }
     }
-    this.allLettersGuessed = function() {
-      return [...this.secretWord].every(secretLetter => this.attempts.includes(secretLetter));
-    };
-    this.isNoMoreAttemptsRemaining = function() {
-      return this.unsuccessfulAttemptCount === 10;
-    }
   }
+  this.allLettersGuessed = function() {
+    return [...this.secretWord].every(secretLetter => this.attempts.includes(secretLetter));
+  };
+  this.isNoMoreAttemptsRemaining = function() {
+    return this.unsuccessfulAttemptCount === 10;
+  }
+}
 
-  return {
-    HangmanGame,
-    init
-  }
-}());
+export default {
+  HangmanGame,
+  init
+}
