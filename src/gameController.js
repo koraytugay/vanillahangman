e4d795fe7523ea89517bee0b34af7b664acdd654
koraytugay <ktugay@sonatype.com>
2021-09-22 21:33:46 -0400

    no need for iife in controller

--- src/gameController.js
+++ src/gameController.js
@@ -1,67 +1,65 @@
 import hangmanService from './hangmanService.js';
 
-(function gameController() {
-  const allowedLetters = 'abcdefghijklmnopqrstuvwxyz-';
-  let hangmanGame;
+const allowedLetters = 'abcdefghijklmnopqrstuvwxyz-';
+let hangmanGame;
 
-  hangmanService.init().then(() => {
-    hangmanGame = new hangmanService.HangmanGame();
-    redraw();
+hangmanService.init().then(() => {
+  hangmanGame = new hangmanService.HangmanGame();
+  redraw();
+});
+
+document.querySelector("#new-game-button").addEventListener('click', () => {
+  hangmanGame = new hangmanService.HangmanGame();
+  redraw();
+});
+
+document.addEventListener('keydown', function({key}) {
+  if ([...allowedLetters].includes(key) && !hangmanGame.isNoMoreAttemptsRemaining()) {
+    attemptAndRedraw(key);
+  }
+});
+
+function attemptAndRedraw(letter) {
+  hangmanGame.attempt(letter);
+  redraw();
+}
+
+function createAttemptButton(letter) {
+  const letterButton = document.createElement('button');
+  letterButton.classList.add('guess-letter');
+  Object.assign(letterButton, {
+    textContent: letter,
+    disabled: hangmanGame.allLettersGuessed() ||
+        hangmanGame.isNoMoreAttemptsRemaining() || hangmanGame.attempts.includes(letter)
+  });
+  letterButton.addEventListener('click', () => attemptAndRedraw(letter));
+  return letterButton;
+}
+
+function redraw() {
+  // clear current divs
+  document.querySelector("#letters-container").innerHTML = "";
+  document.querySelector("#secret-word-container").innerHTML = "";
+
+  // create attempt buttons
+  [...allowedLetters].forEach(letter => {
+    document.querySelector("#letters-container").appendChild(createAttemptButton(letter));
   });
 
-  document.querySelector("#new-game-button").addEventListener('click', () => {
-    hangmanGame = new hangmanService.HangmanGame();
-    redraw();
+  // put the correct image based on attempt count
+  Object.assign(document.querySelector("#hangman-image"), {
+    src: hangmanGame.allLettersGuessed() ?
+        `resources/images/youwin.png` : `resources/images/${hangmanGame.unsuccessfulAttemptCount}.png`
   });
 
-  document.addEventListener('keydown', function({key}) {
-    if ([...allowedLetters].includes(key) && !hangmanGame.isNoMoreAttemptsRemaining()) {
-      attemptAndRedraw(key);
-    }
-  });
-
-  function attemptAndRedraw(letter) {
-    hangmanGame.attempt(letter);
-    redraw();
-  }
-
-  function createAttemptButton(letter) {
-    const letterButton = document.createElement('button');
-    letterButton.classList.add('guess-letter');
-    Object.assign(letterButton, {
-      textContent: letter,
-      disabled: hangmanGame.allLettersGuessed() ||
-          hangmanGame.isNoMoreAttemptsRemaining() || hangmanGame.attempts.includes(letter)
-    });
-    letterButton.addEventListener('click', () => attemptAndRedraw(letter));
-    return letterButton;
-  }
-
-  function redraw() {
-    // clear current divs
-    document.querySelector("#letters-container").textContent = "";
-    document.querySelector("#secret-word-container").textContent = "";
-
-    // create attempt buttons
-    [...allowedLetters].forEach(letter => {
-      document.querySelector("#letters-container").appendChild(createAttemptButton(letter));
-    });
-
-    // put the correct image based on attempt count
-    Object.assign(document.querySelector("#hangman-image"), {
-      src: hangmanGame.allLettersGuessed() ?
-          `resources/images/youwin.png` : `resources/images/${hangmanGame.unsuccessfulAttemptCount}.png`
-    });
-
-    // draw the current state of the secret word
-    [...hangmanGame.secretWord].forEach(letter => {
-          const targetLetterDiv = document.createElement('div');
-          Object.assign(targetLetterDiv, {
-            textContent: hangmanGame.attempts.includes(letter) || hangmanGame.isNoMoreAttemptsRemaining() ? letter : '_'
-          });
-          targetLetterDiv.classList.add('target-letter');
-          document.querySelector("#secret-word-container").appendChild(targetLetterDiv);
-        }
-    );
-  }
-})();
+  // draw the current state of the secret word
+  [...hangmanGame.secretWord].forEach(letter => {
+        const targetLetterDiv = document.createElement('div');
+        Object.assign(targetLetterDiv, {
+          textContent: hangmanGame.attempts.includes(letter) || hangmanGame.isNoMoreAttemptsRemaining() ? letter : '_'
+        });
+        targetLetterDiv.classList.add('target-letter');
+        document.querySelector("#secret-word-container").appendChild(targetLetterDiv);
+      }
+  );
+}
